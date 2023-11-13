34c211345a1c8d57c357debf5feea431d47ae58b
Michael <mikemcd.bbs@gmail.com>
2023-11-13 00:38:39 -0500

    Add background graph logic to board

--- knight_sim.js
+++ knight_sim.js
@@ -25,14 +25,35 @@ const Board = () => {
         [null, null, null, null, null, null, null, null]
     ];
 
+    const _graph = new Map();
+
+    const _addPiece = (piece) => {
+        _graph.set(piece, []);
+    }
+
+    const _setMoves = (piece, col, row) => {
+        for (let move of piece.moves) {
+            const newCol = col + move[0];
+            const newRow = row + move[1];
+            if (0 <= newCol && newCol <= 7 && 0 <= newRow && newRow <= 7) {
+                _graph.get(piece).push([newCol, newRow]);
+            }
+        }
+    }
+
     const placePiece = (piece, col, row) => {
         grid[col][row] = piece;
+        _addPiece(piece);
+        _setMoves(piece, col, row);
         return grid;
     }
 
     return {
         get grid() {
             return grid;
+        },
+        get _graph() {
+            return _graph
         },
         placePiece
     }
