8d9ba9cc3406623438017fec421372a6db11ca7c
Michael <mikemcd.bbs@gmail.com>
2023-11-14 18:57:21 -0500

    Refactor module to give piece position property

--- knight_sim.js
+++ knight_sim.js
@@ -1,4 +1,5 @@
 const Knight = () => {
+    const position = null;
     const moves = [
         [1, 2],
         [2, 1],
@@ -10,7 +11,10 @@ const Knight = () => {
         [-2, -1]
     ];
 
-    return { moves };
+    return {
+        position,
+        moves
+    };
 }
 
 const Board = () => {
@@ -25,26 +29,9 @@ const Board = () => {
         [null, null, null, null, null, null, null, null]
     ];
 
-    const _graph = new Map();
-
-    const _addPiece = (piece) => {
-        _graph.set(piece, []);
-    }
-
-    const _setMoves = (piece, col, row) => {
-        for (let move of piece.moves) {
-            const newCol = col + move[0];
-            const newRow = row + move[1];
-            if (0 <= newCol && newCol <= 7 && 0 <= newRow && newRow <= 7) {
-                _graph.get(piece).push([newCol, newRow]);
-            }
-        }
-    }
-
-    const placePiece = (piece, col, row) => {
-        grid[col][row] = piece;
-        _addPiece(piece);
-        _setMoves(piece, col, row);
+    const placePiece = (piece, row, col) => {
+        piece.position = (row, col);
+        grid[row][col] = piece;
         return grid;
     }
 
