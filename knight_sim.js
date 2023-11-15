08e28631720626f476590b1d9b4dffb105bf2d23
Michael <mikemcd.bbs@gmail.com>
2023-11-15 03:38:42 -0500

    Extend functionality; breaks for high steps

--- knight_sim.js
+++ knight_sim.js
@@ -35,14 +35,14 @@ const Board = () => {
         return grid;
     }
 
+    // TODO: Bug with visited vertices causing some solutions to fall through the cracks; children need multiple parents
     const knightMoves = (start, end) => {
         const visited = {};
-        const results = [];
         let queue = [];
         let buffer = [];
-        // Stringify coordinate pair to be used as object key
-        const root = JSON.stringify(start);
-        visited[root] = null;
+        // Stringify coordinate pair to be used as object child
+        const path = JSON.stringify(start);
+        visited[path] = null;
         // Buffer concats to queue to begin loop
         buffer.push(start);
         const moves = [
@@ -56,39 +56,56 @@ const Board = () => {
             [-2, -1]
         ];
         // Buffer allows queue to retrieve all paths of shortest length, instead of just the first
-        while (!buffer.includes(end)) {
+        while (!(JSON.stringify(end) in visited)) {
             queue = queue.concat(buffer);
             buffer = [];
             // Only receives new coordinates after emptying itself; waves of BFS
             while (!(queue.length === 0)) {
                 const [ currentX, currentY ] = queue.shift();
-                // Stringify coordinate pair to reference object key
-                let parent = JSON.stringify([currentX, currentY]);
+                // Stringify coordinate pair to reference object child
+                const parent = JSON.stringify([currentX, currentY]);
                 for (const [ dx, dy ] of moves) {
                     const xPos = currentX + dx;
                     const yPos = currentY + dy;
-                    // Stringify coordinate pair to be used as object key
+                    // Stringify coordinate pair to be used as object child
                     const child = JSON.stringify([xPos, yPos])
-                    if (child === JSON.stringify(end)) {
-                        const path = [end];
-                        // Loop back up visited references to track shortest path
-                        while (parent in visited) {
-                            // Parse coordinate string for final result
-                            path.unshift(JSON.parse(parent));
-                            parent = visited[parent];
+                    // X and Y must be between 0 and 7 and cannot be the origin
+                    if (xPos >= 0 && xPos <= 7 && yPos >= 0 && yPos <= 7 && !(xPos === start[0] && yPos === start[1])) {
+                        if (child in visited) {
+                            visited[child].push(parent);
+                        } else {
+                            visited[child] = [parent];
+                            buffer.push([xPos, yPos]);
                         }
-                        results.push(path);
-                        buffer.push(end);
-                    }
-                    if (xPos >= 0 && xPos <= 7 && yPos >= 0 && yPos <= 7 && !(child in visited)) {
-                        visited[child] = parent;
-                        buffer.push([xPos, yPos]);
                     }
                 }
             }
         }
-        // Returns a 2D array of lists containing shortest paths
-        return results;
+        const dfsUtil = (graph, child, path, result) => {
+            path.unshift(JSON.parse(child));
+            if (!graph[child]) {
+                result.push(path);
+            } else {
+                const parents = graph[child];
+                // Create unique paths for each parent
+                const paths = [];
+                for (let i = 0; i < parents.length; i++) {
+                    // Use shallow copies of main path
+                    paths.push(path.slice());
+                }
+                let j = 0;
+                for (const parent of parents) {
+                    dfsUtil(graph, parent, paths[j], result);
+                    j++;
+                }
+            }
+        }
+        const getResult = () => {
+            const result = [];
+            dfsUtil(visited, JSON.stringify(end), [], result);
+            return result;
+        }
+        return getResult();
     }
 
     return {
@@ -101,7 +118,7 @@ const Board = () => {
 }
 
 const board = Board();
-board.knightMoves([0, 0],[1, 2]);
+console.log(board.knightMoves([3, 3], [4, 3]));
 
 module.exports = {
     Knight,
