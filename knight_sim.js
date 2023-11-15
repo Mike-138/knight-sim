b12e7aab430367b031f4fcd951e18fa41ebb1e78
Michael <mikemcd.bbs@gmail.com>
2023-11-14 20:04:35 -0500

    Implement shortest knight path algorithm

--- knight_sim.js
+++ knight_sim.js
@@ -35,11 +35,56 @@ const Board = () => {
         return grid;
     }
 
+    const knightMoves = (start, end) => {
+        const visited = {};
+        const queue = [];
+        // Stringify coordinate pair to be used as object key
+        const root = JSON.stringify(start);
+        visited[root] = null;
+        queue.push(start);
+        const moves = [
+            [1, 2],
+            [2, 1],
+            [-1, 2],
+            [2, -1],
+            [1, -2],
+            [-2, 1],
+            [-1, -2],
+            [-2, -1]
+        ];
+        while (!queue.isEmpty) {
+            const [ currentX, currentY ] = queue.shift();
+            // Stringify coordinate pair to reference object key
+            let parent = JSON.stringify([currentX, currentY]);
+            for (const [ dx, dy ] of moves) {
+                const xPos = currentX + dx;
+                const yPos = currentY + dy;
+                // Stringify coordinate pair to be used as object key
+                const child = JSON.stringify([xPos, yPos])
+                if (child === JSON.stringify(end)) {
+                    const result = [end];
+                    // Loop back up visited references to track shortest path
+                    while (parent in visited) {
+                        // Parse coordinate string for final result
+                        result.unshift(JSON.parse(parent));
+                        parent = visited[parent];
+                    }
+                    return result;
+                }
+                if (xPos >= 0 && xPos <= 7 && yPos >= 0 && yPos <= 7 && !(child in visited)) {
+                    visited[child] = parent;
+                    queue.push([xPos, yPos]);
+                }
+            }
+        }
+    }
+
     return {
         get grid() {
             return grid;
         },
-        placePiece
+        placePiece,
+        knightMoves
     }
 }
 
