fa34919a6f9707716e65090f6ca623476d3ca7dd
Michael <mikemcd.bbs@gmail.com>
2023-11-25 17:02:21 -0500

    Add basic support for tab indexing

--- src/components.js
+++ src/components.js
@@ -9,11 +9,14 @@ const Cell = () => {
 const Board = () => {
     const container = document.createElement("div");
     container.classList.add("board");
+    let tabIndex = 1;
     for (let row = 0; row < 8; row++) {
         for (let col = 0; col < 8; col++) {
             const cell = Cell();
             cell.dataset.row = `${row}`;
             cell.dataset.col = `${col}`;
+            cell.tabIndex = tabIndex;
+            tabIndex += 1;
             container.append(cell);
         }
     }
