a91bf074cb9247c4352a0667cbcb467eca5881bf
Michael <mikemcd.bbs@gmail.com>
2023-11-25 15:51:24 -0500

    Implement basic components structure and styling

--- src/components.js
+++ src/components.js
@@ -1,3 +1,25 @@
+import "./styles/components.css";
+
 const Cell = () => {
     const container = document.createElement("div");
+    container.classList.add("cell");
+    return container;
+}
+
+const Board = () => {
+    const container = document.createElement("div");
+    container.classList.add("board");
+    for (let row = 0; row < 8; row++) {
+        for (let col = 0; col < 8; col++) {
+            const cell = Cell();
+            cell.dataset.index = `[${row}, ${col}]`;
+            container.append(cell);
+        }
+    }
+    return container;
+}
+
+export {
+    Cell,
+    Board
 }
