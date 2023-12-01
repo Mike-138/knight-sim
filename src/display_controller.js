55ed4253d43007b0d14dcdb8a6985124f692ed03
Michael <mikemcd.bbs@gmail.com>
2023-11-30 20:11:36 -0500

    Add basic functionality to result button

--- src/display_controller.js
+++ src/display_controller.js
@@ -14,6 +14,14 @@ const build = () => {
 
     startButton.addEventListener("click", logic.__addStartHandlers);
     targetButton.addEventListener("click", logic.__addTargetHandlers);
+    resultButton.addEventListener("click", () => {
+        const start = document.getElementById("start");
+        const target = document.getElementById("target");
+        const results = logic.knightMoves([Number(start.dataset.row), Number(start.dataset.col)], [Number(target.dataset.row), Number(target.dataset.col)]);
+        for (const result of results) {
+            console.log(result);
+        }
+    })
 
     document.body.append(
         logic.board,
