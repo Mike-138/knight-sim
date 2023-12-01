48bdfaaea80d99edfe65f38846426509fcbc9e6c
Michael <mikemcd.bbs@gmail.com>
2023-11-30 20:00:28 -0500

    Move knightMoves and util to logic

--- src/logic_controller.js
+++ src/logic_controller.js
@@ -43,8 +43,75 @@ const __addTargetHandlers = () => {
     _addHandlers(__targetHandler);
 }
 
+const _getMoves = (graph, child, path = [], result = []) => {
+    path.unshift(JSON.parse(child));
+    if (!graph[child]) {
+        result.push(path);
+    } else {
+        const parents = graph[child];
+        for (const parent of parents) {
+            // Pass shallow path copy so each DFS call produces a unique path
+            _getMoves(graph, parent, path.slice(), result);
+        }
+    }
+    return result
+}
+
+const knightMoves = (start, target) => {
+    const moves = [
+        [1, 2],
+        [2, 1],
+        [-1, 2],
+        [2, -1],
+        [1, -2],
+        [-2, 1],
+        [-1, -2],
+        [-2, -1]
+    ];
+    const ignore = [];
+    const graph = {};
+    // Stringify coordinate pair to be used as object child
+    const root = JSON.stringify(start);
+    graph[root] = null;
+    let queue = [];
+    // Buffer must pass starting vertex to queue
+    let buffer = [start];
+    // Buffer allows queue to retrieve all paths of shortest length, instead of just the first
+    while (!(JSON.stringify(target) in graph)) {
+        queue = queue.concat(buffer);
+        // Ignore in buffer allows vertices to have multiple parents only if they occur in the same BFS wave
+        for (const child of buffer) {
+            ignore.push(JSON.stringify(child));
+        }
+        buffer = [];
+        // Only receives new coordinates after emptying itself, creating waves of BFS
+        while (!(queue.length === 0)) {
+            const [ currentX, currentY ] = queue.shift();
+            // Stringify coordinate pair to reference object child
+            const parent = JSON.stringify([currentX, currentY]);
+            for (const [ dx, dy ] of moves) {
+                const xPos = currentX + dx;
+                const yPos = currentY + dy;
+                // Stringify coordinate pair to be used as object child
+                const child = JSON.stringify([xPos, yPos])
+                // X and Y must be between 0 and 7 and cannot have been traversed in a previous BFS wave
+                if (xPos >= 0 && xPos <= 7 && yPos >= 0 && yPos <= 7 && !(ignore.includes(child))) {
+                    if (child in graph) {
+                        graph[child].push(parent);
+                    } else {
+                        graph[child] = [parent];
+                        buffer.push([xPos, yPos]);
+                    }
+                }
+            }
+        }
+    }
+    return _getMoves(graph, JSON.stringify(target));
+}
+
 export {
     board,
     __addStartHandlers,
     __addTargetHandlers,
+    knightMoves
 }
